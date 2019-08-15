9617dea3951c93cdc47e6c80d982cbd5db652ddb
Alexander Reitz <areitz@opentext.com>
2019-08-15 20:30:10 +0200

    Add install script for plugins

---
+++ installPlugins.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
